80f285d8128a14b6afd6eeb70a205c8073dee3dd
veerivenkatasaihitesh <veerivenkatasaihitesh@gmail.com>
2026-04-20 13:48:45 +0530

    SQLalchemy

---
+++ SQLalchemy/config.py
@@ -0,0 +1,4 @@
+class Config:
+    SECRET_KEY = 'dev_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:23UEEC0536%40h@localhost/student_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
